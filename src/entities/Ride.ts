42bb73c10c6385bac261e03fdc0913e94305118c
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2019-11-06 19:21:45 +0100

    implement Relationships

--- src/entities/Ride.ts
+++ src/entities/Ride.ts
@@ -3,10 +3,12 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  ManyToOne,
   PrimaryGeneratedColumn,
   UpdateDateColumn
 } from "typeorm";
 import { statusType } from "./../types/types.d";
+import User from "./User";
 
 @Entity()
 class Ride extends BaseEntity {
@@ -41,6 +43,13 @@ class Ride extends BaseEntity {
 
   @Column({ type: "text" })
   duration: string;
+
+  @ManyToOne(type => User, user => user.rideAsPassenger)
+  passenger: User
+
+  @ManyToOne(type => User, user => user.rideAsDriver)
+  driver: User
+
 
   @CreateDateColumn() createdAt: string;
   @UpdateDateColumn() updatedAt: string;
